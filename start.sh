f354cca733b6af1ad5aa682a674862db36b66b84
vallsCalimaDev <valls@calimasolutions.com>
2025-04-21 16:24:47 +0100

    start.sh y demás archivos

--- start.sh
+++ start.sh
@@ -4,3 +4,5 @@ npm expo install @react-navigation/native @react-navigation/native-stack
 npx expo install react-native-screens react-native-safe-area-context
 
 npx expo install expo-image-picker #imagenes subir
+
+npm install react-native-dropdown-picker
